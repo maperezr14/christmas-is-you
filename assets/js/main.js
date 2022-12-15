77fc3b794c17813b4fcb7112a53b9a6d1302e769
Yolags <yoleanny.garcia@gmail.com>
2022-12-16 00:04:04 +0100

    merge yola

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
         $(this).toggleClass('active');
         $(".navbar").toggleClass("active");
     });
+    /* Agregar acordeon aqui */
+    var acc = document.getElementsByClassName('faqs-title');
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener('click', function () {
+            this.classList.toggle('active');
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + 'px';
+            }
+        });
+    }
 });
